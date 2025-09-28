57c2bf2fffc31f01019fcdb8b24f24e3a7348f5a
shamlaessabahram <s236155@dtu.dk>
2025-09-28 12:44:08 +0200

    færdig version

---
+++ SparePartsInventoryAssistant/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine("Hej. Welcome to the spare parts inventory! ");
+bool partAvailable = false;
+
+while (!partAvailable)
+{
+    Console.Write("Which part do you need? ");
+    string input = Console.ReadLine();
+    
+    if (input == "hydraulic pump" || input == "PLC module" || input == "servo motor")
+    {
+        Console.WriteLine($"I have got {input} here for you. Bye!");
+        partAvailable = true;
+    }
+   
+    else if (input == "Do you actually have any parts?" || input == "Is there anything in stock at all?")
+    {
+        Console.WriteLine("""
+                          We have 3 part(s)!
+                          hydraulic pump
+                          PLC module
+                          servo motor
+                          """);
+    }
+
+    else                                                                                    
+    {
+        Console.WriteLine($"I am afraid we don’t have any {input} in the inventory");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
